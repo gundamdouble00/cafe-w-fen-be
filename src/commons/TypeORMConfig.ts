784c25c8001b23a0dfff94ecbd514128c83557b6
gundamdouble00 <nguyenngocminhquocp@gmail.com>
2026-01-16 11:01:44 +0700

    change in TypeORMConfig.ts

--- src/commons/TypeORMConfig.ts
+++ src/commons/TypeORMConfig.ts
@@ -1,21 +1,11 @@
 import { registerAs } from '@nestjs/config';
-import { config as dotenvConfig } from 'dotenv';
 import { DataSource, DataSourceOptions } from 'typeorm';
-import { envFiles } from './Constant';
-
-// if (process.env.NODE_ENV !== 'production') {
-//   dotenvConfig({ path: envFiles }); 
-// }
 
 const config: DataSourceOptions = {
   type: 'postgres',
-  host: process.env.DATABASE_HOST,
-  port: parseInt(process.env.DATABASE_PORT || '5432', 10),
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-  entities: ['dist/**/*.entity{.ts,.js}'],
-  migrations: ['dist/migrations/*{.ts,.js}'],
+  url: process.env.DATABASE_URL,
+  entities: ['dist/**/*.entity.js'],
+  migrations: ['dist/migrations/*.js'],
   synchronize: false,
   ssl: {
     rejectUnauthorized: false,
