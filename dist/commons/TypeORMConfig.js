8cdf22636dbd660910ad49c19f1f8f8a6736c314
BlackSecret164 <tienanhchilang12@gmail.com>
2026-01-17 01:35:43 +0700

    dist update

--- dist/commons/TypeORMConfig.js
+++ dist/commons/TypeORMConfig.js
@@ -2,12 +2,21 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.connectionSource = void 0;
 const config_1 = require("@nestjs/config");
+const dotenv_1 = require("dotenv");
 const typeorm_1 = require("typeorm");
+const Constant_1 = require("./Constant");
+if (process.env.NODE_ENV !== 'production') {
+    (0, dotenv_1.config)({ path: Constant_1.envFiles });
+}
 const config = {
     type: 'postgres',
-    url: process.env.DATABASE_URL,
-    entities: ['dist/**/*.entity.js'],
-    migrations: ['dist/migrations/*.js'],
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT || '5432', 10),
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    entities: ['dist/**/*.entity{.ts,.js}'],
+    migrations: ['dist/migrations/*{.ts,.js}'],
     synchronize: false,
     ssl: {
         rejectUnauthorized: false,
