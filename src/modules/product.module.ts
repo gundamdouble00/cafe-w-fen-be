0a0a181af1875e071bec858c4364a94e7ea1d4d9
BlackSecret164 <tienanhchilang12@gmail.com>
2026-01-24 04:50:12 +0700

    add rating for product module

--- src/modules/product.module.ts
+++ src/modules/product.module.ts
@@ -5,9 +5,10 @@ import { ProductService } from '../services/product.service';
 import { ProductController } from '../controllers/product.controller';
 import { ProductSize } from '../entities/product_size.entity';
 import { ProductBranch } from 'src/entities/product_branch.entity';
+import { Rating } from 'src/entities/rating.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product, ProductSize, ProductBranch])],
+  imports: [TypeOrmModule.forFeature([Product, ProductSize, ProductBranch, Rating])],
   controllers: [ProductController],
   providers: [ProductService],
   exports: [TypeOrmModule],
